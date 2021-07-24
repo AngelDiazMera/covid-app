ac54beac6753d328c910818691fb17b222ea7d9f
Jorge Angel Diaz Mera <angel_diaz_mera@outlook.com>
2021-07-23 19:11:41 -0500

    google services added

--- lib/services/firebase/push_notification_service.dart
+++ lib/services/firebase/push_notification_service.dart
@@ -1,7 +1,3 @@
-// MD5: 1A:1D:EB:4C:83:C6:5A:BF:CA:57:84:3D:CF:39:E6:9A
-// SHA1: 00:D1:B2:BD:A8:C0:AB:29:34:91:36:CC:46:BD:61:20:DA:69:FD:BC
-// SHA-256: A7:1E:CE:2F:DE:52:80:59:DC:D7:57:C2:7C:7A:FC:43:0A:4A:63:0C:FB:DD:5F:1B:9A:A9:E4:BD:95:E9:6D:F3
-
 import 'dart:async';
 
 import 'package:firebase_core/firebase_core.dart';
